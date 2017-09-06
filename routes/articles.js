df28861b242faef66792a360c1671b72530916e4
huoguangxuan <18618110533@163.com>
2017-09-06 16:16:59 +0800

    thirsday

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router =express.Router();
 var Article =require('../models/article')
 router.get('/add',function(req,res){
-	res.render('add_article',{title:'add artcile'});
+	res.render('add_article',{title:'Add Article'});
 })
 
 //add submit POST router
@@ -13,6 +13,25 @@ router.post('/add',function(req,res){
 	article.author=req.body.author;
 	article.body=req.body.body;
 	article.save(function(err){
+		if(err){
+			console.log(err);
+			return;
+		}else{
+			res.redirect('/');
+		}
+	})
+})
+
+
+//update submit POST router
+
+router.post('/edit/:id',function(req,res){
+	let article ={};
+	article.title=req.body.title;
+	article.author=req.body.author;
+	article.body=req.body.body;
+	let query={_id:req.params.id}
+	Article.update(query,function(err){
 		if(err){
 			console.log(err);
 			return;
--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,24 @@ router.get('/',function(req,res){
 	})
 	
 })
+// get single article router
+router.get('/article/:id',function(req,res){
+	Article.findById(req.params.id,function(err,article){
+		res.render('article',{
+			article:article
+		})
+		return;
+	})
+})
+
+// edit article router
+router.get('/article/edit/:id',function(req,res){
+	Article.findById(req.params.id,function(err,article){
+		res.render('edit_article',{
+			title:'Edit Article',
+			article:article
+		})
+		return;
+	})
+})
 module.exports = router;
